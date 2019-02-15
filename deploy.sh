a3d5e61d97608d90b65d4bb05e971ff5f99e668d
AaronCoplan <afcoplan@gmail.com>
2019-02-14 22:28:23 -0500

    Removed ACM Labs stuff from deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,16 @@
 #!/usr/bin/env bash
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
-sudo apt install -y nodejs
-
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update
-sudo apt install -y yarn
+set -e
 
 mkdir deployment
 cd deployment
 
 git clone https://github.com/gw-acm/acm_site
-git clone https://github.com/gw-acm/acm_labs_site
-
-cd acm_labs_site
-yarn install
-yarn build
-
-cp -r build/ ../acm_site/labs
-cd ..
 
 sudo rm -rf /var/www/html/*
 
 sudo cp -r acm_site/* /var/www/html
 cd ..
 rm -rf deployment/
+
+echo "Site Deployed Successfully!"
